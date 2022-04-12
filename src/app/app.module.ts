0f6043caebbf704abb9831007e442c0757e32f2c
brngpp <giuseppe.bruniani@gmail.com>
2022-04-12 11:07:44 +0200

    nuovo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutenticazioneComponent } from './autenticazione/autenticazione.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutenticazioneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/autenticazione/autenticazione.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticazione',
+  templateUrl: './autenticazione.component.html',
+  styleUrls: ['./autenticazione.component.css']
+})
+export class AutenticazioneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
