54b84e35a52239a0cf4172488e24464faa32b3cb
Jerome Branchetti <93258692+JeromeBranchetti@users.noreply.github.com>
2022-04-13 14:46:02 +0200

    routing basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchPageComponent } from './search/search-page/search-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticazioneComponent } from './autenticazione/autenticazione.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SearchPageComponent},
+  {path: 'home', component: SearchPageComponent},
+  {path: 'edit', component: AdminPageComponent},
+  {path: 'login', component: AutenticazioneComponent},
+  {path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
