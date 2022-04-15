a4dd48eada94ae2e445ad369e130bbb758e1cea1
brngpp <giuseppe.bruniani@gmail.com>
2022-04-15 11:18:47 +0200

    .

--- src/app/search/result/result.component.ts
+++ src/app/search/result/result.component.ts
@@ -2,7 +2,7 @@ import { Ser } from './../ser';
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable, OnInit } from '@angular/core';
 import { Data } from 'src/app/data.model';
-import { TmplAstRecursiveVisitor } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root',
@@ -13,100 +13,58 @@ import { TmplAstRecursiveVisitor } from '@angular/compiler';
   styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit {
-<<<<<<< HEAD
+
   vet:string[]=[]; //vettore stringhe della ricerca
   vetD:Data[]=[]; // vettore dati filtrati dal db
-=======
-  vet: string[] = []; //array di stringhe della ricerca
-  vetD: Data[] = []; //array dei dati filtrati dal db
->>>>>>> da2f7d23c8ba57b1f3ca87e5d6dc4d11601e4a88
 
   constructor(private http: HttpClient, private ser: Ser) {}
 
-<<<<<<< HEAD
-  setDati(dati:Data[]){
-     for(let i of this.vetD){
+
+  setDati(dati:Data[]){      
+    for(let i of this.vetD){
     for(let j of dati){
        if(i.id===j.id){
         
-         return 
+        return 
        
-       }
+      }
 
-     }}
-    this.vetD.push(...dati);
+      }}
+     this.vetD.push(...dati);
     
-  }
+   }
    
   
-  getDati(){
+   getDati(){
    
      console.log("sono in getDati")
       
-    for(let  i=0 ; i<this.vet.length;i++){
+     for(let  i=0 ; i<this.vet.length;i++){
      console.log("dentro il for")
       let url="http://localhost:3000/ricerca"
-      
-      this.http.get<Data[]>(url,{params:{
+     
+     this.http.get<Data[]>(url,{params:{
          q:this.vet[i]
        }}).subscribe(data=>{
         
-        this.setDati(data);
+         this.setDati(data);
        
         
-    })}
-  console.log("fineGetDati")
-  console.log("vetD")
+     })}
+   console.log("fineGetDati")
+   console.log("vetD")
   console.log(this.vetD)
- }
-    avvio(s:string[]){
-      this.vetD=[];
-      console.log(s)
-      this.vet=s;
-      this.getDati();
-=======
-  setDati(dati: Data[]) {
-    for (let i of this.vetD) {
-      for (let j of dati) {
-        if (i.id === j.id) {
-          return;
-        }
-      }
->>>>>>> da2f7d23c8ba57b1f3ca87e5d6dc4d11601e4a88
-    }
-    this.vetD.push(...dati);
   }
 
-  getDati() {
-    console.log('sono in getDati');
-
-    for (let i = 0; i < this.vet.length; i++) {
-      console.log('dentro il for');
-      let url = 'http://localhost:3000/ricerca';
-
-      this.http
-        .get<Data[]>(url, {
-          params: {
-            q: this.vet[i],
-          },
-        })
-        .subscribe((data) => {
-          this.setDati(data);
-          //promise
-        });
-    }
-    console.log('fineGetDati');
-    console.log('vetD');
-    console.log(this.vetD);
-  }
+  
   avvio(s: string[]) {
-    this.vetD = [];
+   this.vetD = [];
     console.log(s);
-    this.vet = s;
-    this.getDati();
-  }
+     this.vet = s;
+     this.getDati();
+   }
 
   ngOnInit(): void {
     this.ser.Emit.subscribe((item) => this.avvio(item));
   }
-}
+    }
